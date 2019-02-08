a8d51518ac8b67b635b5c5ec7520c1ccd97010e5
tonmoy-sardar <tonmoy.1984@gmail.com>
2019-02-08 11:29:00 +0530

    latest code

--- src/core/global.ts
+++ src/core/global.ts
@@ -1,5 +1,5 @@
 export const environment = {
-   apiEndpoint: "http://125.22.105.181:14205/mach_mangso_more/api/",
-   imageBaseUrl: "http://125.22.105.181:14205/mach_mangso_more/uploads/",
+   apiEndpoint: "http://192.168.28.59/mach_mangso_more/api/",
+   imageBaseUrl: "http://192.168.28.59/mach_mangso_more/uploads/",
 
 };
