902ceb53ee8706b71ebe499b64781eb6aa9cc865
Tonmoy Sardar <tonmoy.1984@gmail.com>
2019-02-21 15:16:03 +0530

    update

--- src/pages/layout/orderhistory/orderhistory.ts
+++ src/pages/layout/orderhistory/orderhistory.ts
@@ -5,6 +5,7 @@ import { environment } from '../../../core/global';
 import { ToastController } from 'ionic-angular';
 import { SpinnerDialog } from '@ionic-native/spinner-dialog';
 import { ProductService } from '../../../core/services/product.service';
+import { CartService } from '../../../core/services/cart.service';
 /**
  * Generated class for the OrderhistoryPage page.
  *
@@ -22,6 +23,8 @@ export class OrderhistoryPage {
   orderList:any=[];
   imageBaseUrl:any;
   visibleKey: boolean;
+  productList;
+  customer_cart_data: any;
 
   constructor(
     public navCtrl: NavController,
@@ -30,12 +33,14 @@ export class OrderhistoryPage {
     public menuCtrl: MenuController,
     private toastCtrl: ToastController,
     private spinnerDialog: SpinnerDialog,
-    public productService: ProductService
+    public productService: ProductService,
+    public cartService: CartService
   ) {
     //Header Show Hide Code 
     events.publish('hideHeader', { isHeaderHidden: false, isSubHeaderHidden: false });
     this.imageBaseUrl = environment.imageBaseUrl;
     this.userId = +localStorage.getItem('userId');
+    this.customer_cart_data = [];
   }
 
   ionViewDidLoad() {
@@ -61,6 +66,68 @@ export class OrderhistoryPage {
 
   gotoOrderDetails(id) {
     this.navCtrl.push('OrderdetailsPage',{id: id});
+  }
+
+  repeatOrder(order)
+  {
+   
+    this.spinnerDialog.show();
+    var productIds: any = [];
+    order.order_details.forEach(x => {
+      productIds.push(x.product_id)
+    })
+    
+    var ids = productIds.toString();
+   
+    
+    this.productService.getRepeatOrder(ids).subscribe(
+      res => {
+       this.productList = res['result'];
+       
+       for(var i=0; i<this.productList.length; i++)
+       {
+        var index = order.order_details.findIndex(x => x.product_id == this.productList[i].id);
+        var quantity = order.order_details[index].quantity;
+        var totalOurPrice =   (this.productList[i].price * quantity);
+        var totalMarketPrice =   (this.productList[i].market_price * quantity);
+        var totalSavings =   (totalMarketPrice - totalOurPrice);
+        var data = {
+          customer_id: this.userId,
+          product_id: this.productList[i].id,
+          product_name: this.productList[i].name,
+          description: this.productList[i].description,
+          price: this.productList[i].price,
+          market_price: this.productList[i].market_price,
+          quantity_count: this.productList[i].quantity_count,
+          unit: this.productList[i].unit,
+          image_small: this.productList[i].image_small,
+          quantity: quantity,
+          totalOurPrice: totalOurPrice,
+          totalMarketPrice: totalMarketPrice,
+          totalSavings: totalSavings
+        }
+
+        this.customer_cart_data.push(data);
+        this.setCartData();
+        this.cartService.cartNumberStatus(true);
+        if(i==this.productList.length-1)
+        {
+          this.navCtrl.push('CartPage');
+        }
+
+       }
+
+       this.spinnerDialog.hide();
+      },
+      error => {
+        this.spinnerDialog.hide();
+        
+      }
+    )
+  }
+
+  setCartData() {
+    sessionStorage.setItem("cart", JSON.stringify(this.customer_cart_data));
   }
 
 }
